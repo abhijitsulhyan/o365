5ae59c94da691c5d95f1c433dd52110f84576904
Abhijit Sulhyan <arsulhyan@ciphercloud.com>
2019-01-19 18:03:33 +0530

    Init

--- Demos/03-add-msgraph/graph-tutorial/graph.js
+++ Demos/03-add-msgraph/graph-tutorial/graph.js
@@ -1,24 +1,72 @@
 var graph = require('@microsoft/microsoft-graph-client');
+var fs = require('fs');
 
 module.exports = {
-  getUserDetails: async function(accessToken) {
+    getUserDetails: async function(accessToken) {
+        const client = getAuthenticatedClient(accessToken);
+
+        const user = await
+        client.api('/me').get();
+        return user;
+    },
+
+    getEvents: async function(accessToken) {
+        const client = getAuthenticatedClient(accessToken);
+
+        const events = await
+        client
+            .api('/me/events')
+            .select('subject,organizer,start,end')
+            .orderby('createdDateTime DESC')
+            .get();
+
+        return events;
+    },
+
+    loadFiles: async function(accessToken, count) {
     const client = getAuthenticatedClient(accessToken);
+        for (var i = 0; i < 10; i++) {
 
-    const user = await client.api('/me').get();
-    return user;
-  },
+            var fileName = 'test' + i + '.txt';
+            var path = './' + fileName;
 
-  getEvents: async function(accessToken) {
-    const client = getAuthenticatedClient(accessToken);
+            var data = "O365 Testing " + i;
 
-    const events = await client
-      .api('/me/events')
-      .select('subject,organizer,start,end')
-      .orderby('createdDateTime DESC')
-      .get();
+            var buffer = new Buffer(data + "some content\n");
 
-    return events;
-  }
+            fs.open(path, 'w', function(err, fd) {
+                if (err) {
+                    throw 'error opening file: ' + err;
+                }
+
+                fs.write(fd, buffer, 0, buffer.length, null, function(err) {
+                    if (err) throw 'error writing file: ' + err;
+                    fs.close(fd, function() {
+                        console.log('file written');
+
+                    })
+
+
+                });
+            });
+
+
+            let stream = fs.createReadStream(path); //path to local file
+
+            const events = await client
+                .api('/me/drive/root:/jan18:/children/' + fileName +'/content')
+                .putStream(stream, (err) => {
+                console.log(err);
+            })
+            ;
+
+
+
+        }
+
+    }
+
+
 };
 
 function getAuthenticatedClient(accessToken) {
@@ -31,5 +79,5 @@ function getAuthenticatedClient(accessToken) {
     }
   });
 
-  return client;
+    return client;
 }
